51e99c26c7a71c730db90259913ac9b62ea7796d
Melissa F <melissa.faulner@gmail.com>
2020-10-09 15:21:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,9 @@ def line(katz_deli)
   else 
     line_list = "The line is currently:"
     katz_deli.each_with_index do |value, index| 
-    line_list += " #{index.to_i+1}. #{value}"
-  end 
-  puts "#{line_list}"
+      line_list += " #{index.to_i+1}. #{value}"
+    end 
+    puts "#{line_list}"
   end
 end
 
@@ -18,6 +18,14 @@ end
 #   puts "Welcome, #{customer}. You are number #{katz_deli.size} in line."
 #   return katz_deli
 # end
+
+$counter = 0
+def take_a_number(katz_deli)
+  $counter += 1
+  katz_deli << $counter
+  puts katz_deli
+  puts "Welcome. You are number #{$counter} in line."
+end
 
 def now_serving(katz_deli)
   if katz_deli.empty?
